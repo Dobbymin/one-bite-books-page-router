e16fd015386d3cdd410e6e89d97cbc509e73e5ce
Dobbymin <gmin990624@gmail.com>
2025-09-03 19:06:26 +0900

    feat: 책 검색 기능 구현

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,13 +1,22 @@
 import { useRouter } from 'next/router';
 
-import { SearchbarLayout } from '@/features';
+import { SearchbarLayout, useGetSearchBook } from '@/features';
+import { BookItem } from '@/shared';
 
 export default function SearchPage() {
   const router = useRouter();
+  const q = router.query.q;
+
+  const { data: booksData, isLoading } = useGetSearchBook(q as string);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (booksData?.length === 0) return <div>검색 결과가 없습니다.</div>;
 
   return (
     <div>
-      <h1>검색: {router.query.q}</h1>
+      {booksData?.map((book) => (
+        <BookItem key={`search-${book.id}`} {...book} />
+      ))}
     </div>
   );
 }
